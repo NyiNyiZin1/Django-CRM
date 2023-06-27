2a660f930e441796106d4d96a305771aca416ad4
nyinyizin <nyinyizin@isgm2.com>
2023-06-27 15:42:24 +0900

    admin customize admin

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Record
 
-admin.site.register(Record)
+#customize django admin
+class CustomizeAdmin(admin.ModelAdmin):
+    fields = ['first_name','last_name','address']
+    search_fields = ['first_name','last_name','address']
+    list_filter = ['first_name','last_name','address']
+    list_display = ['first_name','last_name','address']
+    list_editable = ['address']
+
+# Don't Customize Admin
+# admin.site.register(Record)
+# CustomizeAdmin
+admin.site.register(Record,CustomizeAdmin)
